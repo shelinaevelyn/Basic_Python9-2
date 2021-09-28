988762a05a81ab8dcfa4008bc63c4736cc56fca6
Nike Apriliana <nikeapriliana2404@gmail.com>
2021-09-28 16:48:31 +0700

    Pertemuan 1

---
+++ Contoh1.py
@@ -0,0 +1,27 @@
+# print("Hello, world!")
+# print("Hello, world!")
+# print("Hello, world!")
+# print("Hello, world!")
+# ini adalah sebuah komentar
+
+x = 4 #int
+y = "Hello world" #string
+z = 4.2 #float
+var_bool = True #bool
+
+# print(x)
+# print (y)
+# print(type(var_bool))
+
+a = 2
+b = 5
+
+#mod itu %
+#exp atau pangkat itu **
+print(5**2)
+
+#casting itu adalah konversi tipe data
+angka = "100"
+kon = int(angka)
+
+print(type(kon))
